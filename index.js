c4f0ac579c6164643a49de20f5fc5b5d3abcbcde
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-25 22:21:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file.
+function lowerCase(name) {
+  return name.toLocaleLowerCase()
+}
+
 function lowerCaseDrivers(drivers) {
   return drivers.map(toLocaleLowerCase);
 }
