bdf8f631d31b48b9c1e557225e1d2c6c2a05b34b
clark.buskey@gmail.com <clark.buskey@gmail.com>
2018-07-25 22:16:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file.
-function lowerCaseDrivers(name) {
- const drivers = []
- 
+function lowerCaseDrivers(name1, name2, name3) {
+ const drivers = [name1,name2,name3]
+ const driverNames = drivers.map()
 }
